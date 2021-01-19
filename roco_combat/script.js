3f0f2ececb2b51939df984239a77201cd0f21106
lchzh3473 <x54835607@163.com>
2021-01-20 02:22:06 +0800

    窗口适配

--- roco_combat/script.js
+++ roco_combat/script.js
@@ -1,18 +1,26 @@
 "use strict";
 const title = '洛克王国战斗模拟'
-const version = [1, 0, 2];
+const version = [1, 0, 3];
 const firstUpdate = 1606394205088;
-const lastUpdate = 1609864733983;
+const lastUpdate = 1611080357852;
 //检测Flash插件，参考http://lizux.bokee.com/541040.html
 var iFlash;
 var vFlash;
-if (navigator.plugins) {
-	for (var i = 0; i < navigator.plugins.length; i++) {
-		if (navigator.plugins[i].name.toLowerCase().indexOf("shockwave flash") >= 0) {
+var flashInfo = document.getElementById("flashInfo");
+let a = navigator.plugins;
+if (a) {
+	for (var i = 0; i < a.length; i++) {
+		if (a[i].name.toLowerCase().indexOf("shockwave flash") >= 0) {
+			let b = a[i].description;
 			iFlash = true;
-			vFlash = navigator.plugins[i].description.substring(navigator.plugins[i].description.toLowerCase().lastIndexOf("flash ") + 6, navigator.plugins[i].description.length);
+			vFlash = b.substring(b.toLowerCase().lastIndexOf("flash ") + 6, b.length);
 		}
 	}
 }
-if (iFlash == null) document.getElementById("FlashInfo").innerHTML = `<strong>此浏览器未安装或已禁用Flash，该脚本无法运行。</strong>`;
-else document.getElementById("FlashInfo").innerHTML = `<strong style="color: green">当前Flash版本：${vFlash}</strong>`;
+if (iFlash == null) {
+	flashInfo.className = 'error';
+	flashInfo.innerHTML = `此浏览器未安装或已禁用Flash，该脚本无法运行。`;
+} else {
+	flashInfo.className = 'accept';
+	flashInfo.innerHTML = `当前Flash版本：${vFlash}`;
+}
