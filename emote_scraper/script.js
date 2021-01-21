57f2694703a43ec41618c5c5e3cf94e22913acfc
lchzh3473 <x54835607@163.com>
2021-01-21 22:32:25 +0800

    commit

--- emote_scraper/script.js
+++ emote_scraper/script.js
@@ -70,20 +70,24 @@ function convert() {
 	output.innerHTML = `解析成功。(${endTime}s)`;
 	let arr = panelNew.packages;
 	for (let i in arr) {
+		let span1=document.createElement("span");
+		span1.id=`spann${arr[i].id}`;
+		span1.className="spann fold";
 		let img0 = document.createElement("img");
 		img0.id = `img${arr[i].id}`;
-		img0.className = "img fold";
+		img0.className = "img";
 		img0.title = `${(`000${arr[i].id}`).slice(-3)}_${arr[i].text}`;
 		img0.src = `${arr[i].url}@56w_56h.webp`;
-		img0.onclick = function() {
-			document.getElementById(`img${arr[i].id}`).classList.toggle("fold");
-			document.getElementById(`img${arr[i].id}`).classList.toggle("unfold");
+		span1.onclick = function() {
+			document.getElementById(`spann${arr[i].id}`).classList.toggle("fold");
+			document.getElementById(`spann${arr[i].id}`).classList.toggle("unfold");
 			document.getElementById(`span${arr[i].id}`).classList.toggle("hide");
 		}
 		let span0 = document.createElement("span");
 		span0.id = `span${arr[i].id}`;
 		span0.className = "hide";
-		document.getElementById("stage").appendChild(img0);
+		span1.appendChild(img0);
+		document.getElementById("stage").appendChild(span1);
 		document.getElementById("stage").appendChild(span0);
 	}
 	let arr1 = panelNew.statics;
