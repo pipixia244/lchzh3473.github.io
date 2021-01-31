42735ee57cf60b68262d3708cf0259d8e4c92c08
lchzh3473 <x54835607@163.com>
2021-01-31 14:10:01 +0800

    update midi2json

--- midi2json_v3/script.js
+++ midi2json_v3/script.js
@@ -1,9 +1,18 @@
 "use strict";
-const _i = ['MIDI转JSON', [3, 0], 1585107102, 1611980857];
-const pt2Notes = ["A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "#A-3", "B-3", "C-2", "#C-2", "D-2", "#D-2", "E-2", "F-2", "#F-2", "G-2", "#G-2", "A-2", "#A-2", "B-2", "C-1", "#C-1", "D-1", "#D-1", "E-1", "F-1", "#F-1", "G-1", "#G-1", "A-1", "#A-1", "B-1", "c", "#c", "d", "#d", "e", "f", "#f", "g", "#g", "a", "#a", "b", "c1", "#c1", "d1", "#d1", "e1", "f1", "#f1", "g1", "#g1", "a1", "#a1", "b1", "c2", "#c2", "d2", "#d2", "e2", "f2", "#f2", "g2", "#g2", "a2", "#a2", "b2", "c3", "#c3", "d3", "#d3", "e3", "f3", "#f3", "g3", "#g3", "a3", "#a3", "b3", "c4", "#c4", "d4", "#d4", "e4", "f4", "#f4", "g4", "#g4", "a4", "#a4", "b4", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "mute"];
+const _i = ['MIDI转JSON', [3, 0], 1585107102, 1612073331];
+const out = document.getElementById("output");
+const div = document.getElementById("track");
+/*result start*/
+const data = {};
+const tempo = [];
+const tracks = [];
+const drum = [];
+/*result end*/
+var test; //test
 document.getElementById("upload").onchange = function() {
 	const file = this.files[0];
 	document.getElementById("filename").value = file ? file.name : "";
+	wipeData();
 }
 Uint8Array.prototype.getStr = function(start, length) {
 	const end = start + length;
@@ -33,31 +42,34 @@ Uint8Array.prototype.getUint = function(start, length) {
 	return num;
 }
 
-function convert() {
+function wipeData() {
+	tempo.length = 0; //清空数据
+	tracks.length = 0;
+	drum.length = 0;
+	out.innerHTML="";
+	while (div.hasChildNodes()) {
+		div.removeChild(div.firstChild);
+	}
+}
+
+function analyse() {
 	const start = new Date().getTime();
 	const minvol = document.getElementById("volume").value;
 	const single = document.getElementById("single").checked;
-	const out = document.getElementById("output");
 	const file = document.getElementById("upload").files[0];
-	/*result start*/
-	const data = {};
-	const tempo = [];
-	const tracks = [];
-	const drum = [];
-	/*result end*/
 	if (!file) {
 		err("未选择任何文件");
 		return;
 	}
 	const reader = new FileReader();
 	reader.readAsArrayBuffer(file);
-	reader.onprogress = function(progress) { //显示加载文件进度
+	reader.onprogress = (progress) => { //显示加载文件进度
 		let size = file.size;
 		out.className = "accept";
 		out.innerHTML = `加载中：${Math.floor(progress.loaded / size * 100)}%`;
 	};
 	reader.onload = function() {
-		console.clear(); //test
+		wipeData();
 		const midi = new Uint8Array(this.result);
 		let p = 0;
 		if (midi.getStr(p, 4) == "MThd") { //暂未添加文件长度小于4错误信息
@@ -150,16 +162,16 @@ function convert() {
 				}
 			}
 			if (single) tracks.push(track);
-			tempo.sort(arrange); //按tick排序，同时计算绝对时间
+			tempo.sort((a, b) => a.tick - b.tick); //按tick排序，同时计算绝对时间
 			for (let i = 0; i < tempo.length; i++) {
 				let j = tempo[i];
 				let k = tempo[i - 1];
 				j.duration = (j.tick - (k ? k.tick : 0)) * (k ? k.tempo : 0) + (k ? k.duration : 0);
 			}
-			drum.sort(arrange);
+			drum.sort((a, b) => a.tick - b.tick);
 			calcDur(drum);
 			for (const i of tracks) {
-				i.sort(arrange);
+				i.sort((a, b) => a.tick - b.tick);
 				calcDur(i);
 			}
 			data.bpm = 6e7 / (data.maxdur - data.mindur) * (data.maxtick - data.mintick);
@@ -168,33 +180,170 @@ function convert() {
 			err("不是有效的midi文件！");
 			return;
 		}
-		/**/
-		console.log(data); //test
-		console.log(tracks);
-		console.log(drum);
-		console.log(tempo); //test
-		/**/
+		for (const i in tracks) {
+			let option = document.createElement("option");
+			option.innerHTML = single ? "Track" : i;
+			option.value = i;
+			document.getElementById("track").appendChild(option);
+		}
+		if (drum.length) {
+			let option = document.createElement("option");
+			option.innerHTML = "Drum";
+			option.value = -1;
+			document.getElementById("track").appendChild(option);
+		}
 		const end = new Date().getTime();
 		out.className = "accept";
-		out.innerHTML = `转换成功。(${(end - start) / 1000}s)<br><br>时长:&nbsp;${Math.round(data.dur*1000)/1000}s&emsp;BPM:&nbsp;${Math.round(data.bpm*1000)/1000}`;
-	}
-
-	function calcDur(obj) {
-		let num = 0;
-		for (const k of obj) {
-			while (k.tick >= (tempo[num + 1] ? tempo[num + 1].tick : Infinity)) num++;
-			k.duration = tempo[num].duration + (k.tick - tempo[num].tick) * tempo[num].tempo;
-			data.mindur = Math.min(data.mindur, k.duration);
-			data.maxdur = Math.max(data.maxdur, k.duration);
+		out.innerHTML = `分析成功(${(end - start) / 1000}s)：可转换${tracks.length+!!drum.length}条音轨<br>Duration:&nbsp;${Math.round(data.dur*1000)/1000}s&emsp;BPM:&nbsp;${Math.round(data.bpm*1000)/1000}`;
+		/*test1*/
+		function calcDur(obj) {
+			let num = 0;
+			for (const k of obj) {
+				while (k.tick >= (tempo[num + 1] ? tempo[num + 1].tick : Infinity)) num++;
+				k.duration = tempo[num].duration + (k.tick - tempo[num].tick) * tempo[num].tempo;
+				data.mindur = Math.min(data.mindur, k.duration);
+				data.maxdur = Math.max(data.maxdur, k.duration);
+			}
 		}
 	}
+}
 
-	function arrange(a, b) {
-		return a.tick - b.tick;
+function convert() {
+	if (document.getElementById("track").value == "") {
+		err("请先点击“分析”按钮");
+		return;
 	}
+	const start = new Date().getTime();
+	const minnote = document.getElementById("note").value;
+	const tracksss = div.value;
+	test = (tracksss > -1) ? tracks[tracksss] : drum;
+	var bpm = data.bpm; //以后可能添加自定义bpm
+	var minp = Math.round(data.mindur * bpm / data.time / 6e7 * minnote) * 32 / minnote;
+	var maxp = Math.round(data.maxdur * bpm / data.time / 6e7 * minnote) * 32 / minnote;
+	var result = []; //储存结果
+	var notes = []; //储存多重音符
+	var dur = 0; //储存延时
+	var flag = 0; //储存剩余休止符个数
+	for (const i in test) {
+		test[i].p = Math.round(test[i].duration * bpm / data.time / 6e7 * minnote) * 32 / minnote; //[K]/[P]=32
+		if (!test[i - 1]) { //单独考虑第一个音符
+			dur += test[i].p - minp;
+			if (dur > 0)
+				result.push({
+					notes: [128],
+					p: dur
+				});
+			dur = 0;
+			if (test[i].type) {
+				notes.push(test[i].note);
+				flag++;
+			}
+		} else { //考虑非第一个音符
+			dur += test[i].p - test[i - 1].p; //前后时间差
+			if (test[i].type) { //on事件
+				if (dur) { //不为零，则前一音符事件结束
+					result.push({
+						notes: unique(notes),
+						p: dur
+					});
+					flag = 0;
+					dur = 0;
+					notes = [];
+				}
+				notes.push(test[i].note); //加入事件
+				flag++;
+			} else if (flag > 0)
+				flag--; //off事件
+			else if (dur && notes.length) { //flag为零，前一音符事件结束
+				result.push({
+					notes: unique(notes),
+					p: dur
+				});
+				flag = 0;
+				dur = 0;
+				notes = [];
+			}
+		}
+	}
+	dur = maxp - test[test.length - 1].p;
+	if (dur > 0) result.push({
+		notes: [128],
+		p: dur
+	});
+	/*test1-2*/
+	const pt2Note = ["A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "A-3", "#A-3", "B-3", "C-2", "#C-2", "D-2", "#D-2", "E-2", "F-2", "#F-2", "G-2", "#G-2", "A-2", "#A-2", "B-2", "C-1", "#C-1", "D-1", "#D-1", "E-1", "F-1", "#F-1", "G-1", "#G-1", "A-1", "#A-1", "B-1", "c", "#c", "d", "#d", "e", "f", "#f", "g", "#g", "a", "#a", "b", "c1", "#c1", "d1", "#d1", "e1", "f1", "#f1", "g1", "#g1", "a1", "#a1", "b1", "c2", "#c2", "d2", "#d2", "e2", "f2", "#f2", "g2", "#g2", "a2", "#a2", "b2", "c3", "#c3", "d3", "#d3", "e3", "f3", "#f3", "g3", "#g3", "a3", "#a3", "b3", "c4", "#c4", "d4", "#d4", "e4", "f4", "#f4", "g4", "#g4", "a4", "#a4", "b4", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "c5", "mute"];
+	let text = "";
+	result.forEach((item, i, arr) => {
+		if (item.notes.length > 1) {
+			const notes = [];
+			for (const j of item.notes)
+				notes.push(pt2Note[j]);
+			text += `(${notes.join(".")})[${numToLength(item.p, 1)}],`;
+		} else if (item.notes[0] != 128) text += `${pt2Note[item.notes[0]]}[${numToLength(item.p, 1)}],`;
+		else if (arr[i + 1] && arr[i + 1].notes[0] == 128) arr[i + 1].p += item.p;
+		else text += `${numToLength(item.p, 0)},`;
+	});
+	document.getElementById("result").innerText = text;
+	const end = new Date().getTime();
+	out.className = "accept";
+	out.innerHTML = `转换成功。(${(end - start) / 1000}s)<br>Duration:&nbsp;${Math.round(data.dur*1000)/1000}s&emsp;BPM:&nbsp;${Math.round(data.bpm*1000)/1000}`;
+	/*test2*/
+	function unique(arr) {
+		arr.sort((a, b) => a - b);
+		const uniarr = [];
+		for (const i in arr) {
+			if (arr[i] < 21) arr[i] = 21; //以后可以自行设置
+			if (arr[i] > 108 && arr[i] != 128) arr[i] = 108;
+			if (arr[i] == 128) arr[i] = (arr[i - 1] == undefined) ? 128 : arr[i - 1];
+			if (arr[i] != arr[i - 1]) uniarr.push(arr[i]);
+		}
+		if (!uniarr.length) uniarr.push(128);
+		return uniarr;
+	}
+}
 
-	function err(str) {
-		out.className = "error";
-		out.innerHTML = str;
+function err(str) {
+	out.className = "error";
+	out.innerHTML = str;
+}
+
+function numToLength(num, type) { //type:1-音符,0-休止符
+	let text = "";
+	while (num >= 256) {
+		text += type ? "H" : "Q";
+		num -= 256;
 	}
+	while (num >= 128) {
+		text += type ? "I" : "R";
+		num -= 128;
+	}
+	while (num >= 64) {
+		text += type ? "J" : "S";
+		num -= 64;
+	}
+	while (num >= 32) {
+		text += type ? "K" : "T";
+		num -= 32;
+	}
+	while (num >= 16) {
+		text += type ? "L" : "U";
+		num -= 16;
+	}
+	while (num >= 8) {
+		text += type ? "M" : "V";
+		num -= 8;
+	}
+	while (num >= 4) {
+		text += type ? "N" : "W";
+		num -= 4;
+	}
+	while (num >= 2) {
+		text += type ? "O" : "X";
+		num -= 2;
+	}
+	while (num >= 1) {
+		text += type ? "P" : "Y";
+		num--;
+	}
+	return text;
 }
