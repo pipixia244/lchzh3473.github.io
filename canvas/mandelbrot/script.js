5d6bb32c0f0d0528d0084720c7d3f32adbab7219
lchzh3473 <x54835607@163.com>
2021-03-09 17:04:54 +0800

    commit

--- canvas/mandelbrot/script.js
+++ canvas/mandelbrot/script.js
@@ -1,53 +1,74 @@
 "use strict";
-const _i = ['Mandelbrot Set', [1, 0, 1], 1614954857, 1615216754];
+const _i = ['Mandelbrot Set', [1, 0, 2], 1614954857, 1615280661];
 const canvas = document.getElementById("stage");
 const ctx = canvas.getContext("2d");
 window.addEventListener("resize", resize);
 resize();
-let imgData;
+const imgData = new ImageData(500, 500);
 let dx = 0;
 let dy = 0;
-let px = 0;
-let ts = 2880;
-let maxj = 0;
-init(0, 0, 0);
+let dzoom = 0;
+let rowitr = 255;
+let cr, cg, cb;
+init(0, 0, 0, 'a', "51,0,102");
 //作图
-function init(x, y, power, tsp) {
-	dx = x;
-	dy = y;
-	px = power;
-	imgData = new ImageData(500, 500);
-	if (!tsp) tsp = 360;
-	const fd = 0.008 * 10 ** -power;
+function init(x, y, zoom, maxitr, colorstr) {
+	if (!isNaN(x)) dx = x;
+	if (!isNaN(y)) dy = y;
+	if (!isNaN(zoom)) dzoom = zoom;
+	if (isNaN(maxitr)) maxitr = rowitr * 2 ** dzoom;
+	if (colorstr) {
+		let colors = colorstr.split(/,/);
+		cr = Number(colors[0]);
+		cg = Number(colors[1]);
+		cb = Number(colors[2]);
+	}
+	const mxzoom = 0.008 * 10 ** -dzoom;
 	const data = imgData.data;
-	maxj = 0;
-	for (let i = 0; i < data.length; i += 4) {
-		let a = [0, 0];
-		let j;
-		for (j = 0; j < tsp; j++) {
-			if (a[0] * a[0] + a[1] * a[1] > 4) {
-				const hsl = j * 10;
-				[data[i], data[i + 1], data[i + 2]] = hsltorgb(hsl);
-				break;
-			}
-			a = [a[0] * a[0] - a[1] * a[1] + x + (i / 4 % 500 - 250) * fd, 2 * a[0] * a[1] + y - (Math.floor((i / 4) / 500) - 250) * fd];
+	let avgitr = 0;
+	let limitr = 0;
+	let i = 0;
+	while (i < data.length) {
+		const rx = (i % 2000 / 4 - 250) * mxzoom;
+		const ry = (i / 2000 - 250.5) * mxzoom - rx / 500;
+		let zr = 0;
+		let zi = 0;
+		let itr = 0;
+		while (itr++ < maxitr) {
+			if (zr * zr + zi * zi > 4) break;
+			[zr, zi] = [zr * zr - zi * zi + dx + rx, 2 * zr * zi + dy - ry];
 		}
-		maxj += j;
-		data[i + 3] = 240;
+		if (zr * zr + zi * zi > 4) { //取迭代次数
+			data[i++] = color((itr + cr) % 153); //R
+			data[i++] = color((itr + cg) % 153); //G
+			data[i++] = color((itr + cb) % 153); //B
+		} else {
+			data[i++] = 0; //R
+			data[i++] = 0; //G
+			data[i++] = 0; //B
+		}
+		avgitr += itr;
+		if (limitr < itr) limitr = itr;
+		data[i++] = 240; //A
 	}
 	document.getElementById("x").innerText = dx.toFixed(14);
 	document.getElementById("y").innerText = dy.toFixed(14);
-	document.getElementById("px").innerText = `10^${px.toFixed(1)}`;
-	document.getElementById("ts").innerText = `${Math.round(maxj/250000)}/${tsp}`;
+	document.getElementById("px").innerText = `10^${dzoom.toFixed(1)}`;
+	document.getElementById("ts").innerText = `${Math.floor(avgitr/250000)}/${Math.floor(limitr)}`;
+
+	function color(num) {
+		let hsl = num % 1530 * 10;
+		return hsl < 255 ? hsl : hsl < 765 ? 255 : hsl < 1020 ? 1020 - hsl : 0;
+	}
 }
 draw();
 //适配PC
 canvas.addEventListener("mousedown", e => {
 	e.preventDefault();
-	let kx = dx + (e.offsetX - canvas.offsetWidth / 2) * 0.008 * 10 ** -px;
-	let ky = dy + (canvas.offsetHeight / 2 - e.offsetY) * 0.008 * 10 ** -px;
+	let kx = dx + (e.offsetX - canvas.offsetWidth / 2) * 0.008 * 10 ** -dzoom;
+	let ky = dy + (canvas.offsetHeight / 2 - e.offsetY) * 0.008 * 10 ** -dzoom;
 	console.log(e.button);
-	init(kx, ky, px += e.button ? -.1 : .1, ts);
+	init(kx, ky, dzoom += e.button ? -.1 : .1, rowitr * 2 ** dzoom);
 });
 document.oncontextmenu = e => e.returnValue = false;
 //适配移动设备
@@ -74,13 +95,10 @@ canvas.addEventListener("touchstart", evt => {
 canvas.addEventListener("touchend", evt => {
 	evt.preventDefault();
 	for (const i of evt.changedTouches) {
-		/*const idx = i.identifier;
-		item.push(new point(tmp[idx].x1, tmp[idx].y1, (tmp[idx].x1 - tmp[idx].x2) / 20, (tmp[idx].y1 - tmp[idx].y2) / 20, tmp[idx].r, tmp[idx].color));
-		if (idx >= 0) tmp[idx] = {};*/
 		let tm = new Date().getTime() - tmp[i.identifier];
-		let kx = dx + (i.pageX - canvas.offsetLeft - canvas.offsetWidth / 2) * 0.008 * 10 ** -px;
-		let ky = dy + (canvas.offsetHeight / 2 - i.pageY + canvas.offsetTop) * 0.008 * 10 ** -px;
-		init(kx, ky, px += tm > 200 ? -.1 : .1, ts);
+		let kx = dx + (i.pageX - canvas.offsetLeft - canvas.offsetWidth / 2) * 0.008 * 10 ** -dzoom;
+		let ky = dy + (canvas.offsetHeight / 2 - i.pageY + canvas.offsetTop) * 0.008 * 10 ** -dzoom;
+		init(kx, ky, dzoom += tm > 200 ? -.1 : .1, rowitr * 2 ** dzoom);
 	}
 });
 //test
@@ -92,13 +110,4 @@ function draw() {
 function resize() {
 	canvas.width = 500 //window.innerWidth * window.devicePixelRatio;
 	canvas.height = 500 //window.innerHeight * window.devicePixelRatio;
-}
-
-function hsltorgb(num) {
-	return [color(num % 1530), color((num + 510) % 1530), color((num + 1020) % 1530)];
-
-	function color(n) {
-		let m = n > 765 ? 1530 - n : n;
-		return m < 255 ? 255 : m > 510 ? 0 : 510 - m;
-	}
 }
