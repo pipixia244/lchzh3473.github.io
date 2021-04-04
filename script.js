189219248f120c9a95c8ccce4294208890bcada4
lchzh3473 <x54835607@163.com>
2021-04-05 01:38:05 +0800

    commit

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 "use strict";
 window.onload = function() {
+	if (!location.search) location.search = new Date().getTime();
 	if (typeof(_i) == "undefined" || _i.length != 4) return;
 	let d = "lch\zh3473";
 	let w = `作者：<a style="text-decoration:underline"target="_blank"href="https://space.bilibili.com/274753872">${d}</a>`;
 	document.title = `${_i[0]} - ${d}制作`;
-	for(const i of document.querySelectorAll(".title"))i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
-	for(const i of document.querySelectorAll(".info"))i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
+	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
+	for (const i of document.querySelectorAll(".info")) i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
 	document.getElementById("main").style.display = "";
 };
 
